5badcde146d230ed1b3f29279209364e6f5ce7d3
Patrick Stephan <pstephan1187@gmail.com>
2019-01-11 22:07:06 -0600

    Added linting.

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  extends: [
+    "eslint:recommended",
+    'plugin:vue/recommended'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    "indent": ["error", 2],
+    "quotes": ["error", "single"],
+    "semi": ["error", "never"],
+    "space-before-blocks": ["error", "always"],
+    "vue/max-attributes-per-line": ["error", { "singleline": 5 }],
+    "vue/singleline-html-element-content-newline": "off",
+    "import/no-unresolved": "ignore",
+    "comma-dangle": ["error", "never"],
+    "vue/html-closing-bracket-spacing": ["error", {
+      "selfClosingTag": "always"
+    }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel'
+import { eslint } from 'rollup-plugin-eslint'
 
 export default {
   input: 'src/index.js',
@@ -13,6 +14,7 @@ export default {
     'vue-styled-components'
   ],
   plugins: [
+    eslint(),
     babel({
       exclude: 'node_modules/**',
       presets: ['@babel/preset-env'],
