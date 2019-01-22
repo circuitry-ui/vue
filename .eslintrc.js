4a278c69d96ee3dde6150138454e893a2157aa0d
Patrick Stephan <pstephan1187@gmail.com>
2019-01-22 10:12:50 -0600

    Linted the lint file :)

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,10 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module'
   },
   extends: [
-    "eslint:recommended",
+    'eslint:recommended',
     'plugin:vue/recommended'
   ],
   // required to lint *.vue files
@@ -19,16 +19,16 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    "indent": ["error", 2],
-    "quotes": ["error", "single"],
-    "semi": ["error", "never"],
-    "space-before-blocks": ["error", "always"],
-    "vue/max-attributes-per-line": ["error", { "singleline": 5 }],
-    "vue/singleline-html-element-content-newline": "off",
-    "import/no-unresolved": "ignore",
-    "comma-dangle": ["error", "never"],
-    "vue/html-closing-bracket-spacing": ["error", {
-      "selfClosingTag": "always"
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'space-before-blocks': ['error', 'always'],
+    'vue/max-attributes-per-line': ['error', { 'singleline': 5 }],
+    'vue/singleline-html-element-content-newline': 'off',
+    'import/no-unresolved': 'ignore',
+    'comma-dangle': ['error', 'never'],
+    'vue/html-closing-bracket-spacing': ['error', {
+      'selfClosingTag': 'always'
     }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
